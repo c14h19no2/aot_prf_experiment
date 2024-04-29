a06007b5b61e3dfb49df7747cd70f292b8eb0d13
shufan <shufan.z.t@gmail.com>
2024-04-29 12:53:48 +0200

    sub2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 import argparse
 import os.path as op
@@ -11,12 +11,13 @@ from session import PRFBarPassSession
 # prefs.hardware['audioLib'] = 'PTB'
 # prefs.hardware['audioLatencyMode'] = 3
 
-parser = argparse.ArgumentParser(description='A Population Receptive Field experiment')
-parser.add_argument('subject', default=None, nargs='?', 
+parser = argparse.ArgumentParser(
+    description='A Population Receptive Field experiment')
+parser.add_argument('--subject', default=None, nargs='?',
                     help='the subject of the experiment, as a zero-filled integer, such as 001, or 04.')
-parser.add_argument('run', default=1, type=int, nargs='?', 
+parser.add_argument('--run', default=1, type=int, nargs='?',
                     help='the run nr of the experimental run, an integer, such as 1, or 99.')
-parser.add_argument('eyelink', default=0, type=int, nargs='?')
+parser.add_argument('eyelink', default=True, type=int, nargs='?')
 
 cmd_args = parser.parse_args()
 subject, run, eyelink = cmd_args.subject, cmd_args.run, cmd_args.eyelink
@@ -35,9 +36,10 @@ output_str = f'sub-{subject}_run-{str(run).zfill(2)}_task-pRF'
 settings_fn = op.join(op.dirname(__file__), 'settings.yml')
 
 session_object = PRFBarPassSession(output_str=output_str,
-                        output_dir=None,
-                        settings_file=settings_fn, 
-                        eyetracker_on=eyetracker_on)
-logging.warn(f'Writing results to: {op.join(session_object.output_dir, session_object.output_str)}')
+                                   output_dir=None,
+                                   settings_file=settings_fn,
+                                   eyetracker_on=eyetracker_on)
+logging.warn(
+    f'Writing results to: {op.join(session_object.output_dir, session_object.output_str)}')
 session_object.run()
 session_object.close()
